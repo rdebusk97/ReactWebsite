356224282c9386227950aa0253d40e958a1b336f
rdebusk97 <rdebusk97@gmail.com>
2022-09-08 20:29:14 -0500

    Navbar work

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -2,13 +2,19 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [click , setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <div>
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                  RYAN DEBUSK
+                  NAV BAR
                 </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                  <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
             </div>
         </nav>
     </div>
